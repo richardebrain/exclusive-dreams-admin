6f0567e47206963320a2113221a3563813b2a195
richardebrain <richardebrain20@gmail.com>
2024-01-20 00:16:52 +0100

    feat: initial dashboard ui setup

---
+++ src/utils/navigations.ts
@@ -0,0 +1,25 @@
+import {
+  HomeIcon,
+  UsersIcon,
+  FolderIcon,
+  CalendarIcon,
+  DocumentDuplicateIcon,
+  ChartPieIcon,
+  FolderPlusIcon,
+  UserGroupIcon,
+  ShoppingCartIcon,
+} from "@heroicons/react/24/outline";
+
+export const navigation = [
+  { name: "Dashboard", href: "/", icon: HomeIcon, current: true },
+  { name: "Products", href: "/products", icon: FolderPlusIcon, current: false },
+  { name: "Users", href: "/users", icon: UserGroupIcon, current: false },
+  { name: "Orders", href: "/orders", icon: ShoppingCartIcon, current: false },
+  //   { name: "Documents", href: "#", icon: DocumentDuplicateIcon, current: false },
+  //   { name: "Reports", href: "#", icon: ChartPieIcon, current: false },
+];
+export const teams = [
+  { id: 1, name: "Heroicons", href: "#", initial: "H", current: false },
+  { id: 2, name: "Tailwind Labs", href: "#", initial: "T", current: false },
+  { id: 3, name: "Workcation", href: "#", initial: "W", current: false },
+];
