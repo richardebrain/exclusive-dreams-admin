af6b6cb07b7b882aae8384d029a4b89a8ba5f1ba
richardebrain <richardebrain20@gmail.com>
2024-02-11 09:42:38 +0100

    feat: solved statuc api route

--- src/app/api/getAllOrders/route.ts
+++ src/app/api/getAllOrders/route.ts
@@ -2,8 +2,7 @@ import { getAllOrders } from "@/utils/firebase";
 import { NextResponse } from "next/server";
 
 export async function GET(request: Request) {
-  const req =  request.url
-  console.log(req, "req")
   const orders = await getAllOrders();
   return NextResponse.json(orders);
 }
+export const dynamic = "force-dynamic";
--- src/app/api/getAllUsers/route.ts
+++ src/app/api/getAllUsers/route.ts
@@ -3,3 +3,4 @@ import { NextResponse } from "next/server";
 export async function GET(){
     return NextResponse.json({message: "Hello World"});
 }
+export const dynamic = "force-dynamic";
