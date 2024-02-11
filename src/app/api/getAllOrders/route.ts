2b4c889cc646ac1e9190cd514c340f45dc56d1b4
richardebrain <richardebrain20@gmail.com>
2024-02-11 09:22:03 +0100

    feat: solved getAll Products issues

--- src/app/api/getAllOrders/route.ts
+++ src/app/api/getAllOrders/route.ts
@@ -1,7 +1,12 @@
 import { getAllOrders } from "@/utils/firebase";
-import { NextRequest, NextResponse } from "next/server";
 
 export async function GET(request: Request) {
+const req = await request.json();
   const orders = await getAllOrders();
-  return Response.json(orders);
+  return  Response.json(
+    { orders },
+    {
+      status: 200,
+    }
+  );
 }
