72822ba94370e943d30022b43ef6b4a67eac3488
richardebrain <richardebrain20@gmail.com>
2024-02-17 08:36:28 +0100

    feat: image error

--- src/app/(dashboard)/(innerLayout)/carousel/page.tsx
+++ src/app/(dashboard)/(innerLayout)/carousel/page.tsx
@@ -120,11 +120,8 @@ const Page = () => {
                     <Image
                       src={img}
                       alt="carousel"
-                      blurDataURL=""
-                      placeholder="blur"
                       className="w-full h-full object-cover "
                       width={800}
-                      fill
                       height={800}
                     />
                   </div>
